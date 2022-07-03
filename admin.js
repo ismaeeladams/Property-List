c93b64eca2e2ff53e45617c4b046717dc32be930
ismaeeladams <adamsismaeel387@gmail.com>
2022-07-03 20:18:50 +0200

    update

--- admin.js
+++ admin.js
@@ -14,102 +14,102 @@ let items = JSON.parse(localStorage.getItem("items"))
       },
       {
         id: 2,
-        title: "Hello",
-        product: "Hotel",
-        location: "Los Angeles",
-        size: "1000 m2",
-        bedrooms: 3,
-        bathrooms: 1,
-        parking: 2,
-        price: 1500000,
+        title: "Excal",
+        product: "Apartment",
+        location: "Las Vegas",
+        size: "2500 m2",
+        bedrooms: 2,
+        bathrooms: 2,
+        parking: 1,
+        price: 30000,
       },
       {
         id: 3,
-        title: "Hello",
+        title: "Popping",
         product: "Hotel",
-        location: "Los Angeles",
-        size: "1000 m2",
-        bedrooms: 3,
-        bathrooms: 1,
-        parking: 2,
-        price: 1500000,
+        location: "Amsterdam",
+        size: "10000 m2",
+        bedrooms: 5,
+        bathrooms: 3,
+        parking: 6,
+        price: 880000,
       },
       {
         id: 4,
-        title: "Hello",
-        product: "Hotel",
+        title: "Axil",
+        product: "House",
         location: "Los Angeles",
-        size: "1000 m2",
+        size: "100 m2",
         bedrooms: 3,
         bathrooms: 1,
         parking: 2,
-        price: 1500000,
+        price: 1500,
       },
       {
         id: 5,
-        title: "Hello",
+        title: "HeelLoop",
         product: "Hotel",
-        location: "Los Angeles",
-        size: "1000 m2",
-        bedrooms: 3,
-        bathrooms: 1,
-        parking: 2,
-        price: 1500000,
+        location: "Canada",
+        size: "10000 m2",
+        bedrooms: 9,
+        bathrooms: 10,
+        parking: 5,
+        price: 7900000,
       },
       {
         id: 6,
-        title: "Hello",
-        product: "Hotel",
+        title: "Zeelo",
+        product: "Apartment",
         location: "Los Angeles",
-        size: "1000 m2",
-        bedrooms: 3,
-        bathrooms: 1,
-        parking: 2,
-        price: 1500000,
+        size: "4500 m2",
+        bedrooms: 1,
+        bathrooms: 2,
+        parking: 0,
+        price: 1500,
       },
       {
         id: 7,
-        title: "Hello",
-        product: "Hotel",
-        location: "Los Angeles",
-        size: "1000 m2",
-        bedrooms: 3,
-        bathrooms: 1,
-        parking: 2,
-        price: 1500000,
+        title: "Quell",
+        product: "Apartment",
+        location: "Washington",
+        size: "100 m2",
+        bedrooms: 4,
+        bathrooms: 2,
+        parking: 1,
+        price: 15000,
       },
       {
         id: 8,
-        title: "Hello",
+        title: "Veowla",
         product: "Hotel",
-        location: "Los Angeles",
-        size: "1000 m2",
-        bedrooms: 3,
-        bathrooms: 1,
-        parking: 2,
-        price: 1500000,
+        location: "Singapore",
+        size: "100000 m2",
+        bedrooms: 10,
+        bathrooms: 15,
+        parking: 30,
+        price: 3000000,
       },
       {
         id: 9,
-        title: "Hello",
-        product: "Hotel",
-        location: "Los Angeles",
-        size: "1000 m2",
-        bedrooms: 3,
-        bathrooms: 1,
-        parking: 2,
+        title: "Deelo",
+        product: "House",
+        location: "Alabama",
+        size: "20000 m2",
+        bedrooms: 4,
+        bathrooms: 2,
+        parking: 1,
         price: 1500000,
       },
       {
         id: 10,
-        title: "Hello",
+        title: "Resh",
         product: "Hotel",
         location: "Los Angeles",
-        size: "1000 m2",
-        bedrooms: 3,
-        bathrooms: 1,
+        size: "10000 m2",
+        bedrooms: 7,
+        bathrooms: 3,
         parking: 2,
-        price: 1500000,
+        price: 150000,
       },
     ];
 function CallItems(items) {
@@ -178,3 +178,36 @@ function delitem(id) {
   CallItems(items);
 }
 
+// sort function
+const byProduct = items.sort((a, b) =>{
+ if (a.product < b.product) return -1;
+ return 1;
+})
+console.log(byProduct);
+
+
+const byBedroom = items.sort((a, b) =>{
+  return a.bedrooms - b.bedrooms;
+})
+console.log(byBedroom);
+
+const byBathroom = items.sort((a, b) =>{
+  return a.bathrooms - b.bathrooms;
+})
+console.log(byBathroom);
+
+const byPrice = items.sort((a, b) =>{
+  return a.price - b.price;
+})
+console.log(byPrice);
+
+// edit function
+function editItem(){
+  document.getElementById("product").value = productID;
+  document.getElementById("title").value = productID;
+  document.getElementById("size").value = productID;
+  document.getElementById("location").value = productID;
+  document.getElementById("bathroom").value = product_desc;
+  document.getElementById("bedroom").value = qty;
+  document.getElementById("price").value = price; 
+}
